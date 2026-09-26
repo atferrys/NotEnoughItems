9e0e364beaad55e659a3cdfa24b5cd08b87d3681
atferrys <61624086+atferrys@users.noreply.github.com>
2026-09-26 13:15:51 +0200

    refactor: Port Enchantment GUI to GuiContainerWidget like Potion GUI

--- src/main/java/codechicken/nei/gui/GuiEnchantmentModifier.java
+++ src/main/java/codechicken/nei/gui/GuiEnchantmentModifier.java
@@ -1,27 +1,37 @@
 package codechicken.nei.gui;
 
+import codechicken.lib.gui.GuiCCButton;
+import codechicken.lib.gui.GuiScrollSlot;
+import codechicken.lib.inventory.container.GuiContainerWidget;
 import codechicken.lib.texture.TextureUtils;
 import codechicken.nei.NEIClientConfig;
 import codechicken.nei.container.ContainerEnchantmentModifier;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.inventory.GuiContainer;
+import codechicken.nei.network.NEIClientPacketHandler;
+import codechicken.nei.util.NEIServerUtils;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-import java.io.IOException;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
 
 import static codechicken.nei.util.NEIClientUtils.translate;
 
-public class GuiEnchantmentModifier extends GuiContainer {
+public class GuiEnchantmentModifier extends GuiContainerWidget {
 
-    ContainerEnchantmentModifier container;
+    private int level = 5;
+    private GuiCCButton levelDown;
+    private GuiCCButton levelUp;
+    private GuiCCButton validationButton;
+    private GuiSlotEnchantments enchantments;
+    private ItemStack lastStack = ItemStack.EMPTY;
+    private boolean lastValidation;
 
     public GuiEnchantmentModifier(InventoryPlayer inventoryplayer, World world) {
-        super(new ContainerEnchantmentModifier(inventoryplayer, world));
-        container = (ContainerEnchantmentModifier) inventorySlots;
-        container.parentscreen = this;
+        super(new ContainerEnchantmentModifier(inventoryplayer, world), 176, 166);
     }
 
     @Override
@@ -30,20 +40,15 @@ public class GuiEnchantmentModifier extends GuiContainer {
         fontRenderer.drawString(translate("enchant.level"), 19, 20, 0x404040);
     }
 
-    protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+    @Override
+    public void drawBackground() {
         GlStateManager.color(1, 1, 1, 1);
         TextureUtils.changeTexture("textures/gui/container/enchanting_table.png");
-        GlStateManager.translate(guiLeft, guiTop, 0);
         drawTexturedModalRect(0, 0, 0, 0, xSize, ySize);
 
-        container.onUpdate(i, j);
-        container.drawSlots(this);
-        container.drawScrollBar(this);
 
-        String levelstring = "" + container.level;
+        String levelstring = Integer.toString(level);
         fontRenderer.drawString(levelstring, 33 - fontRenderer.getStringWidth(levelstring) / 2, 34, 0xFF606060);
-
-        GlStateManager.translate(-guiLeft, -guiTop, 0);
     }
 
 
@@ -54,12 +59,14 @@ public class GuiEnchantmentModifier extends GuiContainer {
         renderHoveredToolTip(mouseX, mouseY);
     }
 
-    public void initGui() {
-        super.initGui();
-
-        buttonList.add(new GuiNEIButton(0, width / 2 - 78, height / 2 - 52, 12, 12, "<"));
-        buttonList.add(new GuiNEIButton(1, width / 2 - 44, height / 2 - 52, 12, 12, ">"));
-        buttonList.add(new GuiNEIButton(2, width / 2 - 80, height / 2 - 15, 50, 12, lockDisplayString()));
+    @Override
+    public void addWidgets() {
+        add(levelDown = new GuiCCButton(10, 31, 12, 12, "<").setActionCommand("levelDown"));
+        add(levelUp = new GuiCCButton(44, 31, 12, 12, ">").setActionCommand("levelUp"));
+        add(validationButton = new GuiCCButton(8, 68, 50, 12, lockDisplayString()).setActionCommand("validation"));
+        add(enchantments = new GuiSlotEnchantments());
+        refreshEnchantments();
+        updateLevelButtons();
     }
 
     private String lockDisplayString() {
@@ -75,43 +82,148 @@ public class GuiEnchantmentModifier extends GuiContainer {
         NEIClientConfig.world.saveNBT();
     }
 
-    protected void actionPerformed(GuiButton guibutton) {
-        if (guibutton.id == 0) {
-            changeLevel(-1);
-        } else if (guibutton.id == 1) {
-            changeLevel(1);
-        } else if (guibutton.id == 2) {
+    @Override
+    public void actionPerformed(String ident, Object... params) {
+        if(ident.equals("levelDown")) {
+            level = Math.max(1, level - 1);
+        } else if(ident.equals("levelUp")) {
+            level = Math.min(10, level + 1);
+        } else if(ident.equals("validation")) {
             toggleEnchantmentValidation();
-            container.updateEnchantmentOptions(validateEnchantments());
-            guibutton.displayString = lockDisplayString();
+            refreshEnchantments();
         }
+        updateLevelButtons();
     }
 
-    private void changeLevel(int i) {
-        container.level += i;
-        buttonList.get(0).enabled = container.level != 1;
-        buttonList.get(1).enabled = container.level != 10;
+    private void updateLevelButtons() {
+        levelDown.setEnabled(level > 1);
+        levelUp.setEnabled(level < 10);
+    }
+
+    private void refreshEnchantments() {
+        lastStack = inventorySlots.getSlot(0).getStack().copy();
+        lastValidation = validateEnchantments();
+        enchantments.refresh(lastStack, lastValidation);
+        validationButton.text = lockDisplayString();
     }
 
     @Override
-    protected void mouseClicked(int i, int j, int k) throws IOException {
-        if (container.clickButton(i, j, k)) {
-            return;
+    public void updateScreen() {
+        super.updateScreen();
+        if(!ItemStack.areItemStacksEqual(lastStack, inventorySlots.getSlot(0).getStack()) || lastValidation != validateEnchantments()) {
+            refreshEnchantments();
         }
-        if (container.clickScrollBar(i, j, k)) {
-            return;
-        }
-
-        super.mouseClicked(i, j, k);
     }
 
-    @Override
-    protected void mouseReleased(int i, int j, int k) {
-        container.mouseUp(i, j, k);
-        super.mouseReleased(i, j, k);
+    private static class EnchantmentOption {
+        private final Enchantment enchantment;
+        private final int state;
+        private final int appliedLevel;
+
+        private EnchantmentOption(Enchantment enchantment, int state, int appliedLevel) {
+            this.enchantment = enchantment;
+            this.state = state;
+            this.appliedLevel = appliedLevel;
+        }
     }
 
-    public FontRenderer getFontRenderer() {
-        return fontRenderer;
+    public class GuiSlotEnchantments extends GuiScrollSlot {
+        private final List<EnchantmentOption> options = new ArrayList<>();
+
+        public GuiSlotEnchantments() {
+            super(60, 14, 108, 57);
+            setSmoothScroll(false);
+            setMargins(0, 0, 0, 0);
+        }
+
+        @Override
+        public int getSlotHeight(int slot) {
+            return 19;
+        }
+
+        @Override
+        protected int getNumSlots() {
+            return options.size();
+        }
+
+        @Override
+        public int scrollbarGuideAlignment() {
+            return 0;
+        }
+
+        @Override
+        public Dimension scrollbarDim() {
+            Dimension dim = super.scrollbarDim();
+            dim.width = 7;
+            return dim;
+        }
+
+        @Override
+        public void drawOverlay(float frame) {
+
+        }
+
+        @Override
+        public void drawBackground(float frame) {
+            drawRect(x, y, x + width, y + height, 0xFF202020);
+            for(int row = 0; row < 3; row++) {
+                drawEntryBackground(x, y + row * 19, width, 1);
+            }
+        }
+
+        private void drawEntryBackground(int x, int y, int width, int state) {
+            TextureUtils.changeTexture("textures/gui/container/enchanting_table.png");
+            GlStateManager.color(1, 1, 1, 1);
+            drawTexturedModalRect(x, y, 0, 166 + 19 * state, width - 30, 19);
+            drawTexturedModalRect(x + width - 30, y, width - 23, 166 + 19 * state, 30, 19);
+        }
+
+        @Override
+        protected void drawSlot(int slot, int x, int y, int mx, int my, float frame) {
+            EnchantmentOption option = options.get(slot);
+            int width = windowBounds().width;
+            drawEntryBackground(x, y, width, option.state);
+            String text = option.enchantment.getTranslatedName(option.appliedLevel == -1 ? level : option.appliedLevel);
+            if(fontRenderer.getStringWidth(text) > width - 6) text = text.replace("Projectile", "Proj");
+            if(fontRenderer.getStringWidth(text) > width - 6) text = text.replace("Protection", "Protect");
+            if(fontRenderer.getStringWidth(text) > width - 6) text = text.replace("Bane of ", "");
+            int colour = option.state == 0 ? 0x685e4a : option.state == 1 ? 0x407f10 : 0xffff80;
+            fontRenderer.drawString(text, x + 4, y + 5, colour);
+        }
+
+        @Override
+        protected void slotClicked(int slot, int button, int mx, int my, int count) {
+            EnchantmentOption option = options.get(slot);
+            if(option.state != 1) {
+                boolean add = option.state != 2;
+                NEIClientPacketHandler.sendModifyEnchantment(Enchantment.getEnchantmentID(option.enchantment), add ? level : 0, add);
+            }
+        }
+
+        public void refresh(ItemStack stack, boolean validate) {
+            int oldSize = options.size();
+            options.clear();
+            if(!stack.isEmpty() && (!validate || stack.getItem().getItemEnchantability(stack) != 0)) {
+                for(Enchantment enchantment : Enchantment.REGISTRY) {
+                    if(enchantment == null || enchantment.type == null || (validate && !enchantment.type.canEnchantItem(stack.getItem()))) {
+                        continue;
+                    }
+                    int state = 0;
+                    int appliedLevel = -1;
+                    int id = Enchantment.getEnchantmentID(enchantment);
+                    if(NEIServerUtils.stackHasEnchantment(stack, id)) {
+                        state = 2;
+                        appliedLevel = NEIServerUtils.getEnchantmentLevel(stack, id);
+                    } else if(validate && NEIServerUtils.doesEnchantmentConflict(NEIServerUtils.getEnchantments(stack), enchantment)) {
+                        state = 1;
+                    }
+                    options.add(new EnchantmentOption(enchantment, state, appliedLevel));
+                }
+            }
+            setMargins(0, 0, options.size() > 3 ? 7 : 0, 0);
+            if(oldSize != options.size()) {
+                percentscrolled = 0;
+            }
+        }
     }
 }
